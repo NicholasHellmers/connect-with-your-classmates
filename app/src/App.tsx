45b0b9afc1c622a4aacd3e964726388613c2c430
Nicholas Hellmers <nihe1661@colorado.edu>
2024-02-05 00:12:30 -0700

    Initial Commit; setup Solidjs - TypeScript - Tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <header>
+        
+      </header>
+    </div>
+  );
+};
+
+export default App;
